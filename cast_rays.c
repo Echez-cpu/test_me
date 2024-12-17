34f9c8a31a575e37db303125d6570d71e39529b8
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-17 16:22:11 +0100

    Update cast_rays.c

--- cast_rays.c
+++ cast_rays.c
@@ -1,17 +1,17 @@
 int generate_rays(t_field_of_view *camera, t_game_data *data)
 {
-	t_cast_ray ray;
+	t_cast_ray view_ray;
 	int		i;
 
 	i = 0;
-	ray = data->ray;
+	view_ray = data->ray;
 	while (i < data->win_width)
 	{
-		fill_raycastg_para(x, &ray, camera);
-		initialize_ray_traversal(&ray, camera);
-		step_through_grid(data, &ray);
-		calculate_line_height(&ray, data, camera);
-		update_texture_pixels(data, &data->texinfo, &ray, x);
+		fill_raycastg_para(x, &view_ray, camera);
+		initialize_ray_traversal(&view_ray, camera);
+		step_through_grid(data, &view_ray);
+		compute_wall_visualization(&view_ray, data, camera);
+		update_texture_pixels(data, &data->texinfo, &view_ray, i);
 		i++;
 	}
 	return (SUCCESS);
