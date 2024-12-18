f0ec9ef6013b7b959b1469224f4d8bbf7f46f441
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-18 18:43:24 +0100

    Update mlx_folder.c

--- mlx_folder.c
+++ mlx_folder.c
@@ -21,7 +21,7 @@ void	reset_img_struct(t_img *img)
 }
 
 
- void	set_texture_pixels(t_data *data, t_img *image, int x, int y)
+ void	set_pixel_color(t_data *data, t_img *image, int x, int y)
 {
 	if (data->texture_pixels[y][x] > 0)
 		write_color_2_pixel(image, x, y, data->texture_pixels[y][x]);
