5d019bdb7b44f858225d417b04e7bab79a6b4d23
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-18 15:00:10 +0100

    Update handle_render.c

--- handle_render.c
+++ handle_render.c
@@ -3,6 +3,6 @@ int	update_graphics(t_game_data *data)
 	data->fisrt_person.moved += move_player(data);
 	if (data->first_person.moved == 0)
 		return (0);
-	render_images(data);
+	draw_graphics(data);
 	return (0);
 }
