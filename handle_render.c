b40351c6a9677dc4407717538ed3caa347c80b01
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-18 17:45:37 +0100

    Update handle_render.c

--- handle_render.c
+++ handle_render.c
@@ -10,8 +10,30 @@ int	update_graphics(t_game_data *data)
 
 void	draw_graphics(t_game_data *data)
 {
-	init_texture_pixels(data);
+	allocate_pixel_memory(data);
 	zero_out_ray(&data->ray);
 	generate_rays(&data->player, data);
 	render_frame(data);
+}
+
+
+void	allocate_pixel_memory(t_game_data *data)
+{
+	int	i;
+
+	if (data->texture_pixels)
+		free_tab((void **)data->texture_pixels);
+	data->texture_pixels = ft_calloc(data->win_height + 1,
+			sizeof * data->texture_pixels);
+	if (!data->texture_pixels)
+		//clean and exit.
+	i = 0;
+	while (i < data->win_height)
+	{
+		data->texture_pixels[i] = ft_calloc(data->win_width + 1,
+				sizeof * data->texture_pixels);
+		if (!data->texture_pixels[i])
+			//clean and exit
+			i++;
+	}
 }
