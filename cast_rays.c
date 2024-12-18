19d0bc318c13411592c8eb40d99b529e44473bfc
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-18 14:09:24 +0100

    Update cast_rays.c

--- cast_rays.c
+++ cast_rays.c
@@ -11,7 +11,16 @@ int generate_rays(t_field_of_view *camera, t_game_data *data)
 		initialize_ray_traversal(&view_ray, camera);
 		step_through_grid(data, &view_ray);
 		compute_wall_visualization(&view_ray, data, camera);
-		update_texture_pixels(data, &data->texinfo, &view_ray, i);
+		map_texture_to_ray_hit(data, &data->texinfo, &view_ray, i);
+
+		//int y = ray.draw_start;
+		//while (y < ray.draw_end)
+		//{
+			// data->texture_pixels[y][x] = (ray.side == 0) ? 0xFF0000 : 0x00FF00; 
+			// y++;
+			// Red for vertical walls, green for horizontal ones 
+		//}	
+		
 		i++;
 	}
 	return (SUCCESS);
@@ -30,3 +39,56 @@ void	initialize_mlx(t_game_data *data)
 	
 	return ;
 }
+
+
+
+void	map_texture_to_ray_hit(t_game_data *data, t_texinfo *txture, t_cast_ray *ray, int x)
+{
+	int			y;
+	int			color;
+
+	set_texture_direction(data, ray);
+	txture->x = (int)(ray->wall_x * txture->size);
+	if ((ray->side == 0 && ray->dir_x < 0)
+		|| (ray->side == 1 && ray->dir_y > 0))
+		txture->x = txture->size - txture->x - 1;
+	txture->step = 1.0 * txture->size / ray->line_height;
+	txture->pos = (ray->draw_start - data->win_height / 2
+			+ ray->line_height / 2) * txture->step;
+	y = ray->draw_start;
+	while (y < ray->draw_end)
+	{
+		txture->y = (int)txture->pos & (txture->size - 1);
+		txture->pos += txture->step;
+		color = data->textures[txture->index][txture->size * teture->y + txture->x];
+		if (txture->index == NORTH || txture->index == EAST)
+			color = (color >> 1) & 8355711;
+		if (color > 0)
+			data->texture_pixels[y][x] = color;
+		y++;
+	}
+}
+
+
+
+
+void	set_texture_direction(t_game_data *data, t_cast_ray *ray)
+{
+	if (ray->side == 0)
+	{
+		if (ray->dir_x < 0)
+			data->texinfo.index = WEST;
+		else
+			data->texinfo.index = EAST;
+	}
+	else
+	{
+		if (ray->dir_y > 0)
+			data->texinfo.index = SOUTH;
+		else
+			data->texinfo.index = NORTH;
+	}
+}
+
+
+
