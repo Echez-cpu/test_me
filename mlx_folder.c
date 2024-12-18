0c4758475881d0890930e1ad7beb5b0221833c00
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-18 18:12:42 +0100

    Update mlx_folder.c

--- mlx_folder.c
+++ mlx_folder.c
@@ -1,1 +1,21 @@
 
+void	setup_image(t_game_data *data, t_img *image, int width, int height)
+{
+	reset_img_struct(image);
+	image->img = mlx_new_image(data->mlx, width, height);
+	if (image->img == NULL)
+		clean_exit(data, err_msg("mlx", ERR_MLX_IMG, 1));
+	image->addr = (int *)mlx_get_data_addr(image->img, &image->pixel_bits,
+			&image->size_line, &image->endian);
+	return ;
+}
+
+
+void	reset_img_struct(t_img *img)
+{
+	img->img = NULL;
+	img->addr = NULL;
+	img->pixel_bits = 0;
+	img->size_line = 0;
+	img->endian = 0;
+}
