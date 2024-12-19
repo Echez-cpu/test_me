27125dd8cd06cc3ad398ea211e7982b1ddb3a8d1
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-19 18:24:01 +0100

    Update camera_movement.c

--- camera_movement.c
+++ camera_movement.c
@@ -5,14 +5,62 @@ int	camera_spin(t_game_data *data)
 
 	moved = 0;
 	if (data->first_person.move_y == 1)
-		moved += move_player_forward(data);
+		moved += propel_forward(data);
 	if (data->first_person.move_y == -1)
-		moved += move_player_backward(data);
+		moved += propel_backward(data);
 	if (data->first_person.move_x == -1)
-		moved += move_player_left(data);
+		moved += glide_left(data);
 	if (data->first_person.move_x == 1)
-		moved += move_player_right(data);
+		moved += glide_right(data);
 	if (data->first_person.rotate != 0)
-		moved += rotate_player(data, data->first_person.rotate);
+		moved += rotate_camera(data, data->first_person.rotate);
 	return (moved);
 }
+
+
+
+static int	propel_forward(t_game_data *data)
+{
+	double	i;
+	double	j;
+
+	i = data->first_person.pos_x + data->first_person.dir_x * MOVESPEED;
+	j = data->first_person.pos_y + data->first_person.dir_y * MOVESPEED;
+	return (validate_move(data, i, j));
+}
+
+
+
+static int	propel_backward(t_game_data *data)
+{
+	double	i;
+	double	j;
+
+	i = data->first_person.pos_x - data->first_person.dir_x * MOVESPEED;
+	j = data->first_person.pos_y - data->first_person.dir_y * MOVESPEED;
+	return (validate_move(data, i, j));
+}
+
+
+static int	glide_left(t_game_data *data)
+{
+	double	i;
+	double	j;
+
+	i = data->first_person.pos_x + data->first_person.dir_y * MOVESPEED;
+	j = data->first_person.pos_y - data->first_person.dir_x * MOVESPEED;
+	return (validate_move(data, i, j));
+}
+
+
+
+static int	glide_right(t_game_data *data)
+{
+	double	i;
+	double	j;
+
+	i = data->first_person.pos_x - data->first_person.dir_y * MOVESPEED;
+	j = data->first_person.pos_y + data->first_person.dir_x * MOVESPEED;
+	return (validate_move(data, i, j));
+}
+
