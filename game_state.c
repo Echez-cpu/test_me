e3c0e0f51f3f881410af0a270f73c652b1ef0c5e
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-19 20:58:20 +0100

    Update game_state.c

--- game_state.c
+++ game_state.c
@@ -1,14 +1,30 @@
-void	setup_game_state(t_data *data)
+void	setup_game_state(t_game_data *data)
 {
 	data->mlx = NULL;
 	data->win = NULL;
 	data->win_height = WIN_HEIGHT;
 	data->win_width = WIN_WIDTH;
-	init_player(&data->player);
-	init_texinfo(&data->texinfo);
+	init_camera(&data->first_person);
+	init_txtureinfo(&data->texinfo);
 	data->map = NULL;
 	init_mapinfo(&data->mapinfo);
 	init_img_clean(&data->minimap);
 	data->texture_pixels = NULL;
 	data->textures = NULL;
+}
+
+
+static void	init_camera(t_player *cam)
+{
+	cam->dir = '\0';
+	cam->pos_x = 0.0;
+	cam->pos_y = 0.0;
+	cam->dir_x = 0.0;
+	cam->dir_y = 0.0;
+	cam->plane_x = 0.0;
+	cam->plane_y = 0.0;
+	cam->moved = 0;
+	cam->move_x = 0;
+	cam->move_y = 0;
+	cam->rotate = 0;
 }
