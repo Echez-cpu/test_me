b5ac8228d289c4ccc1a1795f19e671b04f4c12a0
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-19 20:40:10 +0100

    Create game_state.c

---
+++ game_state.c
@@ -0,0 +1,14 @@
+void	setup_game_state(t_data *data)
+{
+	data->mlx = NULL;
+	data->win = NULL;
+	data->win_height = WIN_HEIGHT;
+	data->win_width = WIN_WIDTH;
+	init_player(&data->player);
+	init_texinfo(&data->texinfo);
+	data->map = NULL;
+	init_mapinfo(&data->mapinfo);
+	init_img_clean(&data->minimap);
+	data->texture_pixels = NULL;
+	data->textures = NULL;
+}
