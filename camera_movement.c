4697f7d27eca698d7838e26797daf69a4cd93f72
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-19 17:06:18 +0100

    Update camera_movement.c

--- camera_movement.c
+++ camera_movement.c
@@ -1,1 +1,18 @@
 
+int	camera_spin(t_data *data)
+{
+	int	moved;
+
+	moved = 0;
+	if (data->player.move_y == 1)
+		moved += move_player_forward(data);
+	if (data->player.move_y == -1)
+		moved += move_player_backward(data);
+	if (data->player.move_x == -1)
+		moved += move_player_left(data);
+	if (data->player.move_x == 1)
+		moved += move_player_right(data);
+	if (data->player.rotate != 0)
+		moved += rotate_player(data, data->player.rotate);
+	return (moved);
+}
