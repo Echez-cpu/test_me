23bf48de46d9619c9a4c274a24d81aa4b09bf00e
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-20 19:10:55 +0100

    Update setup_textures.c

--- setup_textures.c
+++ setup_textures.c
@@ -3,7 +3,7 @@ void	configure_textures(t_game_data *data)
 {
 	data->textures = ft_calloc(5, sizeof * data->textures);
 	if (!data->textures)
-		clean_exit(data, err_msg(NULL, ERR_MALLOC, 1));
+		// clean and exit
 	data->textures[NORTH] = xpm_to_img(data, data->texinfo.north);
 	data->textures[SOUTH] = xpm_to_img(data, data->texinfo.south);
 	data->textures[EAST] = xpm_to_img(data, data->texinfo.east);
