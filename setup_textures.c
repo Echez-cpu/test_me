129e96bc177e6082cd6a01e6d1ecf71ebc54ccbe
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-20 19:19:16 +0100

    Update setup_textures.c

--- setup_textures.c
+++ setup_textures.c
@@ -4,8 +4,37 @@ void	configure_textures(t_game_data *data)
 	data->textures = ft_calloc(5, sizeof * data->textures);
 	if (!data->textures)
 		// clean and exit
-	data->textures[NORTH] = xpm_to_img(data, data->texinfo.north);
-	data->textures[SOUTH] = xpm_to_img(data, data->texinfo.south);
-	data->textures[EAST] = xpm_to_img(data, data->texinfo.east);
-	data->textures[WEST] = xpm_to_img(data, data->texinfo.west);
+	data->textures[NORTH] = get_texture_from_xpm(data, data->texinfo.north);
+	data->textures[SOUTH] = get_texture_from_xpm(data, data->texinfo.south);
+	data->textures[EAST] = get_texture_from_xpm(data, data->texinfo.east);
+	data->textures[WEST] = get_texture_from_xpm(data, data->texinfo.west);
+}
+
+
+static int	*get_texture_from_xpm(t_game_data *data, char *path)
+{
+	t_img	tmp;
+	int		*buffer;
+	int		x;
+	int		y;
+
+	init_texture_img(data, &tmp, path);
+	buffer = ft_calloc(1,
+			sizeof * buffer * data->texinfo.size * data->texinfo.size);
+	if (!buffer)
+		clean_exit(data, err_msg(NULL, ERR_MALLOC, 1));
+	y = 0;
+	while (y < data->texinfo.size)
+	{
+		x = 0;
+		while (x < data->texinfo.size)
+		{
+			buffer[y * data->texinfo.size + x]
+				= tmp.addr[y * data->texinfo.size + x];
+			++x;
+		}
+		y++;
+	}
+	mlx_destroy_image(data->mlx, tmp.img);
+	return (buffer);
 }
