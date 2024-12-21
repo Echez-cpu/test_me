43d553501c2e87792fbbd43fb6be3da88e75b7d5
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-21 16:54:30 +0100

    Update keyboard_handler.c

--- keyboard_handler.c
+++ keyboard_handler.c
@@ -38,3 +38,14 @@ static int	reset_keyPress_flags(int key, t_game_data *press)
 		press->first_person.rotate = 0;
 	return (0);
 }
+
+
+void	setup_input_hooks(t_game_data *data)
+{
+	mlx_hook(data->win, ClientMessage, NoEventMask, end_game, data);
+	mlx_hook(data->win, KeyPress, KeyPressMask, key_press, data);
+	mlx_hook(data->win, KeyRelease, KeyReleaseMask, reset_keyPress_flags, data);
+}
+
+
+
