7a80049a0b9dc13dfc179d9eb34fc26622da09d3
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-21 16:07:23 +0100

    Update camera_movement.c

--- camera_movement.c
+++ camera_movement.c
@@ -4,13 +4,13 @@ int	camera_spin(t_game_data *data)
 	int	moved;
 
 	moved = 0;
-	if (data->first_person.move_y == 1)
+	if (data->first_person.move_y == 5)
 		moved += propel_forward(data);
-	if (data->first_person.move_y == -1)
+	if (data->first_person.move_y == -5)
 		moved += propel_backward(data);
-	if (data->first_person.move_x == -1)
+	if (data->first_person.move_x == -5)
 		moved += glide_left(data);
-	if (data->first_person.move_x == 1)
+	if (data->first_person.move_x == 5)
 		moved += glide_right(data);
 	if (data->first_person.rotate != 0)
 		moved += rotate_camera(data, data->first_person.rotate);
