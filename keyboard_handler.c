67fee52f66d1b0afd0f89d68aa653c563d2e823a
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2024-12-21 16:36:55 +0100

    Update keyboard_handler.c

--- keyboard_handler.c
+++ keyboard_handler.c
@@ -14,11 +14,27 @@ static int	key_press(int key, t_game_data *press)
 	if (key == KEY_RIGHT)
 		press->first_person.rotate += 5;
 	if (key == KEY_ESC)
-		quit_progm(press);
+		end_game(press);
 	return (0);
 }
 
 
 
-
-
+static int	reset_keyPress_flags(int key, t_game_data *press)
+{
+	if (key == KEY_ESC)
+		end_game(press);
+	if (key == KEY_W && press->first_person.move_y == 5)
+		press->first_person.move_y = 0;
+	if (key == KEY_S && press->first_person.move_y == -5)
+		press->first_person.move_y = 0;
+	if (key == KEY_A && press->first_person.move_x == -5)
+		press->first_person.move_x += 5;
+	if (key == KEY_D && press->first_person.move_x == 5)
+		press->first_person.move_x -= 5;
+	if (key == KEY_LEFT && press->first_person.rotate <= 5)
+		press->first_person.rotate = 0;
+	if (key == KEY_RIGHT && press->first_person.rotate >= -5)
+		press->first_person.rotate = 0;
+	return (0);
+}
